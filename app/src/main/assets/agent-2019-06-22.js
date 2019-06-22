68a42ea10229405ac3f178109d48afebc2b64a1b
Giovanni Rocca <giovanni.rocca.90@gmail.com>
2019-06-23 00:36:27 +0200

    update script and apk

--- app/src/main/assets/agent-2019-06-22.js
+++ app/src/main/assets/agent-2019-06-22.js
@@ -1,4 +1,4 @@
-var selinuxFd = 0;
+var selinuxFd = -1;
 
 function strstr(what, which) {
     return what.indexOf(which) >= 0;
@@ -18,9 +18,9 @@ Interceptor.attach(Module.findExportByName(null, 'faccessat'), {
         this.path = path;
     },
     onLeave: function(ret) {
-        if (strstr(this.path, '/system/bin') ||
-            strstr(this.path, '/system/xbin') ||
-            strstr(this.path, '/data/local')) {
+        if (
+            strstr(this.path, '/data/local') ||
+            strstr(this.path, '/system')) {
             log('*filtered*', 'faccessat-ret');
             ret.replace(-1);
         }
@@ -35,6 +35,11 @@ Interceptor.attach(Module.findExportByName(null, 'open'), {
         const path = this.context.x0.readUtf8String();
         log(path, 'open');
         this.path = path;
+
+        if (this.path.indexOf('.apk') >= 0) {
+            this.path = this.path.replace('root', 'xxxx');
+            this.context.x0.writeUtf8String(this.path);
+        }
     },
     onLeave: function(ret) {
         log(ret.toString() + ' - ' + this.path, 'open-ret');
@@ -47,12 +52,12 @@ Interceptor.attach(Module.findExportByName(null, 'open'), {
 Interceptor.attach(Module.findExportByName(null, 'read'), {
     onEnter: function() {
         const fd = this.context.x0;
-        log(fd, 'read');
         this.fd = fd;
         this.buf = this.context.x1;
     },
     onLeave: function(ret) {
         if (parseInt(this.fd) === selinuxFd) {
+            selinuxFd = -1;
             log('*replacing selinux enforce');
             this.buf.writeU8(1);
         }
@@ -63,5 +68,8 @@ Interceptor.attach(Module.findExportByName(null, 'stat64'), {
     onEnter: function() {
         const path = this.context.x0.readUtf8String();
         log(path, 'stat64');
+    },
+    onLeave: function(ret) {
+        ret.replace(-1);
     }
 });
