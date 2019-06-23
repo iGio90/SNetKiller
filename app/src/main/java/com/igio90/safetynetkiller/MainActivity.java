c888150e69651d25fce26e787cc3bee3dfda938d
Giovanni Rocca <giovanni.rocca.90@gmail.com>
2019-06-23 15:29:05 +0200

    retrieve gservices pid with alternate methods if pidof is not in place

--- app/src/main/java/com/igio90/safetynetkiller/MainActivity.java
+++ app/src/main/java/com/igio90/safetynetkiller/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.res.AssetManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -101,8 +102,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void checkFridaInjected() {
-        String res = RootManager.getInstance().runCommand("pidof " + TARGET).getMessage()
-                .replace("\n", "");
+        String res = RootManager.getInstance().runCommand("which pidof").getMessage();
+        if (res.isEmpty()) {
+            res = RootManager.getInstance().runCommand("" +
+                    "for p in /proc/[0-9]*; do [[ $(<$p/cmdline) = "
+                    + TARGET +
+                    " ]] && echo ${p##*/}; done"
+            ).getMessage();
+        } else {
+            res = RootManager.getInstance().runCommand("pidof " + TARGET).getMessage()
+                    .replace("\n", "");
+        }
+
         if (!res.isEmpty()) {
             try {
                 int pid = Integer.parseInt(res);
